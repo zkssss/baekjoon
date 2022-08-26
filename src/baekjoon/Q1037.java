996466eca93fc448f81676a8fd863c8a19cc2a9a
HSM <zack122193@gmail.com>
2022-08-27 02:58:51 +0900

    1037

---
+++ src/baekjoon/Q1037.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Q1037 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Scanner sc= new Scanner(System.in);
+		 int n = sc.nextInt();
+		 List<Integer> list = new ArrayList<Integer>();
+		 for(int i=0;i<n;i++) {
+			 list.add(sc.nextInt());
+		 }
+		 
+		 list.sort(Comparator.reverseOrder());
+		 System.out.println(list.get(0)*list.get(list.size()-1)); 
+	}
+}

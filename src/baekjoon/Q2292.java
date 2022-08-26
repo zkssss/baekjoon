789113e2cf3893f9063c699d8d7e1abdf52a659e
HSM <zack122193@gmail.com>
2022-08-26 15:14:16 +0900

    2292

--- src/baekjoon/Q2292.java
+++ src/baekjoon/Q2292.java
@@ -8,17 +8,17 @@ public class Q2292 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int count = 1;
-		int distance=6;
+		int count=1;
+		int value=2;
 		
-		while(true) {
-			if(n<distance) {
-				System.out.println(count);
-				break;
-			}
+		while(value<=n) {
+			value+=count*6;
 			count++;
-			distance+=6;
 		}
-		
+		if(n==1) {
+			System.out.println(1);
+		}else {
+			System.out.println(count);
+		}
 	}
-}
+} 

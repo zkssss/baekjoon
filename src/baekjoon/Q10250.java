d46c715cc8327bbd45e7866d66952437fd0c5a72
Seoulculture <Seoulculture@192.168.35.47>
2022-09-01 19:34:18 +0900

    10250 notsolved

---
+++ src/baekjoon/Q10250.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q10250 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int i=0;i<t;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int h = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			int n = Integer.parseInt(st.nextToken());
+			while(n<h) {
+				
+			}
+		}
+		
+	}
+}
+

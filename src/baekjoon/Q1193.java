8ff8f63d23256622d511e0c056090225483d7aec
Seoulculture <Seoulculture@192.168.34.28>
2022-09-03 17:08:17 +0900

    Q1193

--- src/baekjoon/Q1193.java
+++ src/baekjoon/Q1193.java
@@ -8,26 +8,31 @@ public class Q1193 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int x = Integer.parseInt(br.readLine());
-		int a=1;
-		int b=1;
-		int count=1;
+		int hap =0;
+		int n=0;
+		int num=0;
+		int a=0;
+		int b=0;
 		
-		for (int i = x; i > 0; i--) {
-			count = count * i;
+		while(x>hap) {
+			//몇번째 대각선
+			n++;
+			//x가 더 클때까지의 합
+			hap = n*(n+1)/2;
 		}
-		System.out.println(count);
+		//이전 대각선까지의 합을 x에서 빼서 해당 대각선에서 몇번째에 해당하는지
+		num = x-n*(n-1)/2;
 		
-		if(a/2==1) {
-			for(int i=0;i<count;i++) {
-				b--;
-				a++;
-			}
-		}else {
-			for(int i=0;i<count;i++) {
-				b++;
-				a--;
-			}
+		//짝수행
+		if(n%2==0) {
+			a=num;
+			b=n-num+1;
+		}else { //홀수행
+			a=n-num+1;
+			b=num;
 		}
 		
-		}
+		System.out.println(a+"/"+b);
+		
+	}
 }

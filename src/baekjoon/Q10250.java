388b4249054798e250870df0f9895dad1a78f317
Seoulculture <Seoulculture@192.168.34.28>
2022-09-03 17:58:18 +0900

    Q10250

--- src/baekjoon/Q10250.java
+++ src/baekjoon/Q10250.java
@@ -3,21 +3,44 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Q10250 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine());
+		int h=0;
+		int w=0;
+		int n=0;
+		List<String> list = new ArrayList<String>();
 		
 		for(int i=0;i<t;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int h = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			int n = Integer.parseInt(st.nextToken());
-			while(n<h) {
-				
+			h = Integer.parseInt(st.nextToken());
+			w = Integer.parseInt(st.nextToken());
+			n = Integer.parseInt(st.nextToken());
+			
+			int room = 0;
+			int floor = 0;
+			if(n%h==0) {
+				room = n/h;
+				floor=h;
+			}else {
+				room = n/h+1;
+				floor = n%h;
 			}
+			String r=String.valueOf(room);
+			if(room<10) {
+				r = "0"+room;
+			}
+			
+			list.add(floor+r);
+		}
+		
+		for(String s : list) {
+			System.out.println(s);
 		}
 		
 	}

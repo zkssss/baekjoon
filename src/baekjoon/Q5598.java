1891a931953ee54d2074f3f2f5f04531bbe41d93
HSM <zack122193@gmail.com>
2022-09-04 22:36:50 +0900

    5598

---
+++ src/baekjoon/Q5598.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q5598 {
+
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		
+		String pwd = sc.next();
+		char[] a = new char[pwd.length()];
+		for(int i=0;i<pwd.length();i++) {
+			a[i] = pwd.charAt(i);
+			int num = (int)a[i];
+			
+			if(num-3<'A') {
+				num+=23;
+			}else {
+				num-=3;
+			}
+			
+			a[i] = (char)num;
+			
+			System.out.print(a[i]);
+		}
+	}
+}

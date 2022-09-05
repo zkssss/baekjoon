3e557894d5688d3a4e88518e9eb23de7e6a9f565
Seoulculture <Seoulculture@192.168.34.28>
2022-09-05 14:55:07 +0900

    2775

---
+++ src/baekjoon/Q2775.java
@@ -0,0 +1,40 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q2775 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		int k=0;
+		int n=0;
+		int num[][]=new int[15][15];
+		List<Integer> list = new ArrayList<Integer>();
+		
+		for(int i=0;i<15;i++) {
+			num[i][1] = 1;
+			num[0][i] = i;
+		}
+		
+		for(int i=1;i<15;i++) {
+			for(int j=2;j<15;j++) {
+				num[i][j] = num[i][j-1] + num[i-1][j];
+			}
+		}
+		
+		for(int i=0;i<t;i++) {
+			k = Integer.parseInt(br.readLine());
+			n = Integer.parseInt(br.readLine());
+			list.add(num[k][n]);
+		}
+		
+		for(int i : list) {
+			System.out.println(i);
+		}
+		
+	}
+}

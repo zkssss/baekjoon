c2cd292c0a46c6dcf537c223ae030cf26480223c
HSM <zack122193@gmail.com>
2022-09-09 16:21:49 +0900

    10757

---
+++ src/baekjoon/Q10757.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Q10757 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] strs = br.readLine().split(" ");
+		BigInteger a = new BigInteger(strs[0]);
+		BigInteger b = new BigInteger(strs[1]);
+		a = a.add(b);
+		System.out.println(a);
+	}
+}

d376a37574aa5670dc02fd806ad06cec09da51e1
HSM <zack122193@gmail.com>
2022-09-11 18:12:00 +0900

    10998

---
+++ src/baekjoon/Q10998.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Q10998 {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a*b);
+	}
+}

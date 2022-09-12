40dd7562987445e9d3302c756237b4b96bd5e515
HSM <zack122193@gmail.com>
2022-09-12 14:48:56 +0900

    9498

---
+++ src/baekjoon/Q9498.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q9498 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int score = sc.nextInt();
+		String point = "";
+		
+		if(score>=90 & score<=100) {
+			point = "A";
+		}else if(score>=80 & score<=89) {
+			point = "B";
+		}else if(score>=70 & score<=79) {
+			point = "C";
+		}else if(score>=60 & score<=69) {
+			point = "D";
+		}else {
+			point = "F";
+		}
+		
+		System.out.println(point);
+	}
+}

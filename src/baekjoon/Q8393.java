ffabb29d58b8740f18d1e4e3c2118caf2cbfa34e
HSM <zack122193@gmail.com>
2022-09-13 18:25:04 +0900

    8393

---
+++ src/baekjoon/Q8393.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q8393 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		System.out.println((n*(n+1))/2);
+	}
+}

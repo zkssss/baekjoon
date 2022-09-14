d640801de17cb4f670a7dbd866a8dd4e6ff7cf89
HSM <zack122193@gmail.com>
2022-09-14 18:10:46 +0900

    11022

---
+++ src/baekjoon/Q11022.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q11022 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i=0;i<t;i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = a+b;
+			System.out.println("Case #"+(i+1)+": "+a+" + "+b+" = "+c);
+		}
+	}
+}

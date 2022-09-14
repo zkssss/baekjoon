426995ec8b54555e14ec43402adb2fb189472e1b
HSM <zack122193@gmail.com>
2022-09-14 18:59:29 +0900

    2439

---
+++ src/baekjoon/Q2439.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q2439 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int count = n;
+		String blank = " ";
+		String star = "*";
+		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<count-1;j++) {
+				System.out.print(blank);
+			}
+			System.out.println(star);
+			count--;
+			star+="*";
+		}
+	}
+}

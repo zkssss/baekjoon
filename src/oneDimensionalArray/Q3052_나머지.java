afa9f3d2a1da6aee48c2c2e4d358166094d3f0c7
HSM <zack122193@gmail.com>
2022-09-16 02:41:10 +0900

    3052_나머지

---
+++ src/oneDimensionalArray/Q3052_나머지.java
@@ -0,0 +1,19 @@
+package oneDimensionalArray;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Q3052_나머지 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Set<Integer> set = new HashSet<Integer>();
+		
+		for(int i=0;i<10;i++) {
+			set.add(sc.nextInt()%42);
+		}
+		
+		System.out.println(set.size());
+		
+	}
+}

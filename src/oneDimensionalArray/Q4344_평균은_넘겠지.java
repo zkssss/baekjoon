6c9d0b0d2cb0393cc0c3a62eaafedb586366f037
HSM <zack122193@gmail.com>
2022-09-16 19:21:53 +0900

    Q4344_평균은_넘겠지

---
+++ src/oneDimensionalArray/Q4344_평균은_넘겠지.java
@@ -0,0 +1,29 @@
+package oneDimensionalArray;
+
+import java.util.Scanner;
+
+public class Q4344_평균은_넘겠지 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int c = sc.nextInt();
+		
+		for(int i=0;i<c;i++) {
+			int n = sc.nextInt();
+			int[][] score = new int[c][n];
+			float hap = 0;
+			float avg = 0;
+			float count = 0;
+			for(int j=0;j<n;j++) {
+				score[i][j] = sc.nextInt();
+				hap+=score[i][j];
+			}
+			avg = hap/n;
+			for(int j : score[i]) {
+				if(j>avg) {
+					count++;
+				}
+			}
+			System.out.println(String.format("%.3f", count/n*100) +"%");
+		}
+	}
+}

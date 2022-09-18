cc294dfcfca51958170266dd180b2eee014598ce
HSM <zack122193@gmail.com>
2022-09-19 00:53:47 +0900

    Q11654_아스키코드

---
+++ src/문자열/Q11654_아스키코드.java
@@ -0,0 +1,10 @@
+package 문자열;
+
+import java.util.Scanner;
+
+public class Q11654_아스키코드 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println((int)(sc.next().charAt(0)));
+	}
+}

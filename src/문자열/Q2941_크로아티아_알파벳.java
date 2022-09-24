7f636f287afac1ae555ce60e62e1aeefcd2aa015
HSM <zack122193@gmail.com>
2022-09-24 16:58:10 +0900

    Q2941_크로아티아_알파벳

--- src/문자열/Q2941_크로아티아_알파벳.java
+++ src/문자열/Q2941_크로아티아_알파벳.java
@@ -1,7 +1,53 @@
 package 문자열;
 
+import java.util.Scanner;
+
 public class Q2941_크로아티아_알파벳 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String word = sc.next();
+		int count = 0;
 		
+		for(int i=0;i<word.length()-1;i++) {
+			if(word.charAt(i) == 'c') {
+				if(i<word.length()-1) {
+					if(word.charAt(i+1) == '=' || word.charAt(i+1) == '-') {
+						count++;
+					}
+				}
+			}else if(word.charAt(i) == 'd') {
+				if(i<word.length()-1) {
+					if(word.charAt(i+1) == 'z') {
+						if(i<word.length()-2) {
+							if(word.charAt(i+2) == '=') {
+								count+=2;
+								i+=2;
+							}
+						}
+						
+					}else if(word.charAt(i+1) == '-') {
+						count++;
+					}
+				}
+			}else if(word.charAt(i)=='l') {
+				if(word.charAt(i+1)=='j') {
+					count++;
+				}
+			}else if(word.charAt(i)=='n') {
+				if(word.charAt(i+1)=='j') {
+					count++;
+				}
+			}else if(word.charAt(i)=='s') {
+				if(word.charAt(i+1)=='=') {
+					count++;
+				}
+			}else if(word.charAt(i)=='z') {
+				if(word.charAt(i+1)=='=') {
+					count++;
+				}
+			}
+		}
+		
+		System.out.println(word.length() - count);
 	}
 }

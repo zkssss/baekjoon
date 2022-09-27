a7815620ddc24ac185cc1c87b0fb0ad80f2a41dc
HSM <zack122193@gmail.com>
2022-09-27 16:49:15 +0900

    Q10250_ACM_호텔

--- src/기본수학1/Q10250_ACM_호텔.java
+++ src/기본수학1/Q10250_ACM_호텔.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Q10250 {
+public class Q10250_ACM_호텔 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine());

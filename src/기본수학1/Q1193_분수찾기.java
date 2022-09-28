5220b95759b67f47c4396aea367a24e0f18fdae6
HSM <zack122193@gmail.com>
2022-09-28 18:29:00 +0900

    Q1193_분수찾기

--- src/기본수학1/Q1193_분수찾기.java
+++ src/기본수학1/Q1193_분수찾기.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Q1193 {
+public class Q1193_분수찾기 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int x = Integer.parseInt(br.readLine());

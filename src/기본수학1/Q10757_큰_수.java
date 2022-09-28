6c6cbc40a6735d383182ed877c7046201d538ba8
HSM <zack122193@gmail.com>
2022-09-28 18:28:45 +0900

    Q10757_큰_수

--- src/기본수학1/Q10757_큰_수.java
+++ src/기본수학1/Q10757_큰_수.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
 
-public class Q10757 {
+public class Q10757_큰_수 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] strs = br.readLine().split(" ");

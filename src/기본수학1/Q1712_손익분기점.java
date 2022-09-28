1698446237651ebbc0ae0e4f24571bbef7df2809
HSM <zack122193@gmail.com>
2022-09-28 18:29:17 +0900

    Q1712_손익분기점

--- src/기본수학1/Q1712_손익분기점.java
+++ src/기본수학1/Q1712_손익분기점.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Q1712 {
+public class Q1712_손익분기점 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] strs = br.readLine().split(" ");

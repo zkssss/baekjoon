b616a5ff4ec4c525eb520a9f937530fa5708c95a
HSM <zack122193@gmail.com>
2022-09-28 18:29:31 +0900

    Q2292_벌집

--- src/기본수학1/Q2292_벌집.java
+++ src/기본수학1/Q2292_벌집.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Q2292 {
+public class Q2292_벌집 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());

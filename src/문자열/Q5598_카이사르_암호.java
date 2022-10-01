a84101cd82131ff253fe1f4c2e5e6e9bebf33a39
HSM <zack122193@gmail.com>
2022-10-01 23:27:44 +0900

    Q5598_카이사르_암호

--- src/문자열/Q5598_카이사르_암호.java
+++ src/문자열/Q5598_카이사르_암호.java
@@ -1,8 +1,8 @@
-package baekjoon;
+package 문자열;
 
 import java.util.Scanner;
 
-public class Q5598 {
+public class Q5598_카이사르_암호 {
 
 	public static void main(String[] args) {
 		Scanner sc =  new Scanner(System.in);

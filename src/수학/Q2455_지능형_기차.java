d9930af6b0180ac1f8e00e958a9565220a49e3b3
HSM <zack122193@gmail.com>
2022-10-02 20:59:56 +0900

    Q2455_지능형_기차

--- src/수학/Q2455_지능형_기차.java
+++ src/수학/Q2455_지능형_기차.java
@@ -1,8 +1,8 @@
-package baekjoon;
+package 수학;
 
 import java.util.Scanner;
 
-public class Q2455 {
+public class Q2455_지능형_기차 {
 	public static void main(String[] args) {
 		
 		Scanner sc =new Scanner(System.in);

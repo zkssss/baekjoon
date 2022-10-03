8c946abf88b8acc3ef813a29715b192efca845a2
HSM <zack122193@gmail.com>
2022-10-03 20:39:39 +0900

    Q2739_구구단

--- src/수학/Q2739_구구단.java
+++ src/수학/Q2739_구구단.java
@@ -1,8 +1,8 @@
-package baekjoon;
+package 수학;
 
 import java.util.Scanner;
 
-public class Q2739 {
+public class Q2739_구구단 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();

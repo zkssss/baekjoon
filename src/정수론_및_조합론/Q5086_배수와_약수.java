984a0d186145ddd49dedcd6bbbe2bfbb25e35daa
SuMin Hong <97527445+zkssss@users.noreply.github.com>
2022-10-17 14:15:11 +0900

    Create Q5086_배수와_약수.java
    Q5086_배수와_약수

---
+++ src/정수론_및_조합론/Q5086_배수와_약수.java
@@ -0,0 +1,31 @@
+package beakJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q5086_배수와_약수 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String msg = "";
+
+		while(true){
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			int num1 = Integer.parseInt(st.nextToken());
+			int num2 = Integer.parseInt(st.nextToken());
+
+			if(num1==0 & num2==0){
+				break;
+			}else if(num2 % num1 == 0){
+				msg+="factor\n";
+			}else if(num1 % num2==0){
+				msg+="multiple\n";
+			}else{
+				msg+="neither\n";
+			}
+		}
+
+		System.out.println(msg);
+	}
+}
